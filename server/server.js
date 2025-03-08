02c1f56b2adc7ef3c653760330f45bb1dd408469
Terra Sarr <samba.co93@gmail.com>
2025-03-08 04:52:18 +0000

    updated files

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
 const app = express();
@@ -17,7 +19,9 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
-app.use('/api/admin',adminRouter)
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
 
 // Port
 const port = process.env.PORT || 5000;
